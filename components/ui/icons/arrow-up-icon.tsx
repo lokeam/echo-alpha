d4d396608c3870edced8cd9582b19dc7c4c70f0c
Ahn Ming Loke <lokeahnming@gmail.com>
2026-01-01 17:24:34 -0500

    feat(ui): add drawer component and icon library

---
+++ components/ui/icons/arrow-up-icon.tsx
@@ -0,0 +1,27 @@
+import { cn } from '@/lib/utils';
+
+type ArrowIconUpProps = {
+  className?: string;
+};
+
+export function ArrowIconUp({ className }: ArrowIconUpProps) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={24}
+      height={24}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth={2}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className={cn("icon icon-tabler icons-tabler-outline icon-tabler-arrow-up", className)}
+    >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+        <path d="M12 5l0 14" />
+        <path d="M18 11l-6 -6" />
+        <path d="M6 11l6 -6" />
+      </svg>
+  );
+}
